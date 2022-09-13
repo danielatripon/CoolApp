8b404a0efa3bb4bb27bc2a7a4ad4068ec8e99343
Daniela Tripon <daniela.tripon29@gmail.com>
2022-09-13 14:20:02 +0100

    Styled Tab navigation. Added icons

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,29 @@
 import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 import { StyleSheet, View, ActivityIndicator, Text } from "react-native";
 import MyText from "../components/MyText";
 import { Image } from "@rneui/themed";
+import { SearchBar } from "@rneui/themed";
 
 const BASE_URI =
   "https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg";
 
 export default function Home() {
+  const [search, setSearch] = useState("");
+
+  const updateSearch = (search) => {
+    setSearch(search);
+  };
   return (
     <View style={styles.container}>
       <MyText> Welcome to my CoolApp!</MyText>
-
+      <View style={styles.searchBar}>
+        <SearchBar
+          placeholder="Type Here..."
+          onChangeText={updateSearch}
+          value={search}
+        />
+      </View>
       <Image
         source={{ uri: BASE_URI }}
         containerStyle={styles.image}
@@ -37,5 +50,8 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "60%",
     marginTop: 50,
+  },
+  searchBar: {
+    width: "90%",
   },
 });
