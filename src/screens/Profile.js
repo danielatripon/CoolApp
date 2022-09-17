0937943b9f4ff62de34f6a99550b3f8fc44d6056
Daniela Tripon <daniela.tripon29@gmail.com>
2022-09-17 20:20:39 +0100

    Profile page created

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,17 +1,46 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { Input, Button } from "@rneui/themed";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import { Input, Button, Image } from "@rneui/themed";
 import { useDispatch } from "react-redux";
 import { Logout } from "../store/actions";
 
+const BASE_URI =
+  "https://www.clipartmax.com/png/middle/277-2772117_user-profile-avatar-woman-icon-avatar-png-profile-icon.png";
 export default function Profile() {
   const dispatch = useDispatch();
   const submit = () => {
     dispatch(Logout());
   };
   return (
-    <View style={StyleSheet.container}>
-      <Button onPress={submit}>Logout</Button>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome back!</Text>
+      <Image
+        source={{ uri: BASE_URI }}
+        containerStyle={styles.image}
+        PlaceholderContent={<ActivityIndicator />}
+      />
+      <Button
+        title="Logout"
+        titleStyle={{ fontWeight: "bold", fontSize: 18 }}
+        buttonStyle={{
+          backgroundColor: "#4cbb17",
+          borderRadius: 5,
+        }}
+        containerStyle={{
+          width: 300,
+          marginHorizontal: 50,
+          marginVertical: 10,
+        }}
+        icon={{
+          name: "arrow-right",
+          type: "font-awesome",
+          size: 15,
+          color: "white",
+        }}
+        iconRight
+        iconContainerStyle={{ marginLeft: 10, marginRight: -10 }}
+        onPress={submit}
+      />
     </View>
   );
 }
@@ -21,5 +50,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     padding: 16,
+    backgroundColor: "#DFFBEE",
+  },
+  buttonStyle: {
+    backgroundColor: "#4cbb17",
+    alignItems: "center",
+    justifyContent: "center",
+    fontWeight: "bold",
+    color: "#0EE310",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    elevation: 3,
+    borderRadius: 4,
+  },
+  image: {
+    backgroundColor: "red",
+    backgroundSize: "cover",
+    width: "100%",
+    height: "60%",
+    marginTop: 50,
+  },
+  title: {
+    color: "#74C5A1",
+    fontWeight: "bold",
+    fontSize: 30,
+    paddingBottom: 20,
+    textAlign: "center",
   },
 });
